2a3e44c6cefb2d14093c9b5d31eca8fd584e17b7
Oscar Stevens <82244060+ostev@users.noreply.github.com>
2022-12-25 03:53:05 +0000

    Build from setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -50,3 +50,9 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+cd /workspaces/roc-retro/
+git submodule init
+git submodule update
+cd roc
+cargo build --release
